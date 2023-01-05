7e830f880ebf187d7432a4672ca96b1b7ea88aef
doccomsa <newcomsa@nate.com>
2023-01-05 17:17:02 +0900

    test

---
+++ src/main/java/com/ezen/demo/SortTest.java
@@ -0,0 +1,18 @@
+package com.ezen.demo;
+
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		int[] nums = {1,10,2,7,4};
+		
+		for(int i=0;i<nums.length;i++) {
+			for(int j=i+1;j<nums.length;j++) {
+				
+			}
+		}
+		for(int i=0;i<nums.length;i++) {
+			System.out.println(nums[i]);
+		}
+	}
+}
