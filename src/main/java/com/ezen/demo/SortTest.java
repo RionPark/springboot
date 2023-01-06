c411e1734dd21b41d451e89db2af4cb8ad6ea072
doccomsa <newcomsa@nate.com>
2023-01-06 09:23:52 +0900

    test

--- src/main/java/com/ezen/demo/SortTest.java
+++ src/main/java/com/ezen/demo/SortTest.java
@@ -1,14 +1,18 @@
 package com.ezen.demo;
 
+import java.util.Arrays;
 
 public class SortTest {
 
 	public static void main(String[] args) {
 		int[] nums = {1,10,2,7,4};
-		
 		for(int i=0;i<nums.length;i++) {
 			for(int j=i+1;j<nums.length;j++) {
-				
+				if(nums[i]>nums[j]) {
+					int tmpNum = nums[i];
+					nums[i] = nums[j];
+					nums[j] = tmpNum;
+				}
 			}
 		}
 		for(int i=0;i<nums.length;i++) {
